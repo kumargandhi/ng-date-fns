659f56164f8516389c4c610a8a2d31b7d0bab858
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-10-11 12:00:07 +0530

    Add the dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format, compareAsc } from 'date-fns'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-date-fns';
+
+  today = new Date();
+
+  _fToday = format(this.today, 'MM/dd/yyyy');
 }
